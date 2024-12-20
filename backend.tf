f2ecf3c08fa1c6c187eebdd776c6c6e89449fb02
matheusrmello <matheus.rm.09@hotmail.com>
2024-12-20 15:52:31 -0300

    Fix the error

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "test-matheus-tfstate"
     key            = "backend/tfstate"
-    dynamodb_table = "test-matheus-terraform-state-backend"
+    # dynamodb_table = "test-matheus-terraform-state-backend"
     region         = "us-east-2"
   }
 }
